0f59d311078f542db45767b25c03cf758ce95cca
kumarsai05 <kumar70367@gmail.com>
2025-08-31 23:26:56 +0000

    Initial work for Lab-2

--- app.js
+++ app.js
@@ -1,2 +1,116 @@
 /* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+  const stocksData = JSON.parse(stockContent);
+  const userData = JSON.parse(userContent);
 
+  function generateUserList(users, stocks) {
+    const userList = document.querySelector('.user-list');
+    userList.innerHTML = '';
+    users.map(({ user, id }) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = user.lastname + ', ' + user.firstname;
+      listItem.setAttribute('id', id);
+      userList.appendChild(listItem);
+    });
+    userList.addEventListener('click', (event) => handleUserListClick(event, users, stocks));
+  }
+
+  function handleUserListClick(event, users, stocks) {
+    const userId = event.target.id;
+    const user = users.find(user => user.id == userId);
+    populateForm(user);
+    renderPortfolio(user, stocks);
+  }
+
+  function populateForm(data) {
+    const { user, id } = data;
+    document.querySelector('#userID').value = id;
+    document.querySelector('#firstname').value = user.firstname;
+    document.querySelector('#lastname').value = user.lastname;
+    document.querySelector('#address').value = user.address;
+    document.querySelector('#city').value = user.city;
+    document.querySelector('#email').value = user.email;
+  }
+
+  function renderPortfolio(user, stocks) {
+    const { portfolio } = user;
+    const portfolioDetails = document.querySelector('.portfolio-list');
+    portfolioDetails.innerHTML = '';
+    portfolio.map(({ symbol, owned }) => {
+      const symbolEl = document.createElement('p');
+      const sharesEl = document.createElement('p');
+      const actionEl = document.createElement('button');
+      symbolEl.innerText = symbol;
+      sharesEl.innerText = owned;
+      actionEl.innerText = 'View';
+      actionEl.setAttribute('id', symbol);
+      portfolioDetails.appendChild(symbolEl);
+      portfolioDetails.appendChild(sharesEl);
+      portfolioDetails.appendChild(actionEl);
+    });
+    portfolioDetails.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        viewStock(event.target.id, stocks);
+      }
+    });
+  }
+
+  function viewStock(symbol, stocks) {
+    const stockArea = document.querySelector('.stock-form');
+    if (stockArea) {
+      const stock = stocks.find(function (s) { return s.symbol == symbol; });
+      document.querySelector('#stockName').textContent = stock.name;
+      document.querySelector('#stockSector').textContent = stock.sector;
+      document.querySelector('#stockIndustry').textContent = stock.subIndustry;
+      document.querySelector('#stockAddress').textContent = stock.address;
+      document.querySelector('#logo').src = `logos/${symbol}.svg`;
+    }
+  }
+
+  generateUserList(userData, stocksData);
+
+  const deleteButton = document.querySelector('#deleteButton');
+  if (deleteButton) {
+    deleteButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      const userId = document.querySelector('#userID').value;
+      const userIndex = userData.findIndex(user => user.id == userId);
+      if (userIndex > -1) {
+        userData.splice(userIndex, 1);
+        generateUserList(userData, stocksData);
+        document.querySelector('#userID').value = '';
+        document.querySelector('#firstname').value = '';
+        document.querySelector('#lastname').value = '';
+        document.querySelector('#address').value = '';
+        document.querySelector('#city').value = '';
+        document.querySelector('#email').value = '';
+        const portfolioDetails = document.querySelector('.portfolio-list');
+        if (portfolioDetails) portfolioDetails.innerHTML = '';
+        ['#stockName','#stockSector','#stockIndustry','#stockAddress'].forEach(sel => {
+          const el = document.querySelector(sel);
+          if (el) el.textContent = '';
+        });
+        const logo = document.querySelector('#logo');
+        if (logo) logo.removeAttribute('src');
+      }
+    });
+  }
+
+  const saveButton = document.querySelector('#saveButton');
+  if (saveButton) {
+    saveButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      const id = document.querySelector('#userID').value;
+      for (let i = 0; i < userData.length; i++) {
+        if (userData[i].id == id) {
+          userData[i].user.firstname = document.querySelector('#firstname').value;
+          userData[i].user.lastname = document.querySelector('#lastname').value;
+          userData[i].user.address = document.querySelector('#address').value;
+          userData[i].user.city = document.querySelector('#city').value;
+          userData[i].user.email = document.querySelector('#email').value;
+          generateUserList(userData, stocksData);
+        }
+      }
+    });
+  }
+});
